c77c3a3d1cb0bf3279de42b822cec247bead181e
Nick Reardon <nickjreardon@gmail.com>
2021-09-21 23:01:53 -0400

    webapack config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+      target: 'http://localhost:3000/',
+      changeOrigin: true
+      }
+    },
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.scss$/,
+        use:[
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html"
+    }),
+  ]
+}
