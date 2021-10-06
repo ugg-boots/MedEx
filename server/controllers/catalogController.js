3609f3bae66cb2e7663111ade2f3fa5f05951533
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-10-05 23:08:13 -0400

    Clean back end code

--- server/controllers/catalogController.js
+++ server/controllers/catalogController.js
@@ -4,63 +4,44 @@ const catalogController = {};
 
 // add middleware
 catalogController.getAllProducts = async (req, res, next) => {
-    const allProductsQuery = 
-        `SELECT product_id, product_name, product_desc, suppliers.supplier_name, unit_price, qty_per_unit FROM catalog
-        INNER JOIN suppliers ON catalog.supplier_id = suppliers.supplier_id`; 
-    try {
-        const products = await pool.query(allProductsQuery);
-        res.locals.products = products.rows
-        } catch (err) {
-            next(err);
-        }
-      next();
+    
+  const allProductsQuery = 
+    `SELECT product_id, product_name, product_desc, suppliers.supplier_name, unit_price, qty_per_unit FROM catalog
+    INNER JOIN suppliers ON catalog.supplier_id = suppliers.supplier_id`; 
+  
+  try {
+    const products = await pool.query(allProductsQuery);
+    res.locals.products = products.rows;
+    next();
+  } 
+    
+  catch (err) {
+    next({
+      log: 'catalogController.getAllProducts: ERROR:' + err.message,
+      message: { err: 'catalogController.getAllProducts: ERROR: Check server logs for details' },
+    });
+  }
 };
 
 catalogController.addNewProduct = async (req, res, next) => {
+
+  const { product_name, product_desc, supplier_name, unit_price, qty_per_unit, max_stock } = req.body;
+  const params = [product_name, product_desc, supplier_name, unit_price, qty_per_unit, max_stock];
+  const newProductQuery = `INSERT INTO catalog (product_name, product_desc, supplier_id, unit_price, qty_per_unit, max_stock) 
+  VALUES ($1, $2, (SELECT supplier_id FROM suppliers WHERE supplier_name = $3), $4, $5, $6)`
+
   try {
-    const { product_name, product_desc, supplier_name, unit_price, qty_per_unit } = req.body;
-    const newProduct = await pool.query(
-      `INSERT INTO catalog (product_name, product_desc, supplier_id, unit_price, qty_per_unit) 
-      VALUES ($1, $2, (SELECT supplier_id FROM suppliers WHERE supplier_name = $3), $4, $5)`, 
-      [product_name, product_desc, supplier_name, unit_price, qty_per_unit]
-    );
+    const newProduct = await pool.query(newProductQuery, params);
     res.locals.newProduct = newProduct;
-  } catch(err) {
-    console.log(err);
-    next(err);
+    next();
+  } 
+  
+  catch(err) {
+    next({
+      log: 'catalogController.addNewProduct: ERROR:' + err.message,
+      message: { err: 'catalogController.addNewProduct: ERROR: Check server logs for details' },
+    });
   }
-  next();
-};
-
-catalogController.deleteProduct = async (req, res, next) => {
-  try {
-    console.log('req body for deleteProduct -->', req.body);
-    const id  = req.body[0];
-    const deletedProduct = await pool.query(
-      `DELETE FROM catalog 
-      WHERE product_id = $1`, 
-      [id]
-    );
-    res.locals.deletedProduct = deletedProduct;
-  } catch(err) {
-    console.log(err);
-    next(err);
-  }
-  next();
-};
-
-catalogController.lowStock = async (req, res, next) => {
-  try {
-    console.log('requesting low stock items')
-    const lowStock = await pool.query(
-      `SELECT max_stock, product_name, inventory.quantity AS quantity FROM catalog INNER JOIN inventory ON catalog.product_id = inventory.product_id`
-    );
-    res.locals.lowStock = lowStock.rows;
-  } catch(err) {
-    console.log(err);
-    next(err);
-  }
-  next();
 };
 
 module.exports = catalogController;
