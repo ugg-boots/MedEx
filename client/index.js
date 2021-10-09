d3f8afc137381315464ad27ae5b88c49c07da9d9
Alan Ye <alanye@Alans-MBP.fios-router.home>
2021-10-09 12:43:03 -0400

    catalog add form

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from './store'
 
