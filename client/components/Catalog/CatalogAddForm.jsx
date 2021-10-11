9c416a45d3cd19fe581667470eff2caa985c19a1
Alan Ye <alanye@alans-mbp.lan>
2021-10-10 21:22:48 -0400

    working table, adding item works for catalog

--- client/components/Catalog/CatalogAddForm.jsx
+++ client/components/Catalog/CatalogAddForm.jsx
@@ -1,105 +1,128 @@
 import React, { useEffect, useState } from 'react';
 import { Typography, Button } from '@material-ui/core';
 import { Autocomplete, TextField, Alert } from '@mui/material'
-import { fetchSuppliers, postCatalog } from '../../slices/catalogSlices'
+import { fetchProducts, fetchSuppliers, postCatalog } from '../../slices/catalogSlices'
 import { useDispatch, useSelector } from 'react-redux'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 function CatalogAddForm (props) {
   
   const dispatch = useDispatch();
 
+  // useSelector to pull out supplierNames from dispatch(fetchSuppliers), need to populate dropdown list in render below:
+  const allSupplierNames = useSelector(state => state.catalog.allSuppliers)
+  // useSelector to pull out all product items, needed to check for duplicates already in db
+  const allProducts = useSelector(state => state.catalog.allCatalogItems)
+
+  // useState for modal open/close
+  const [open, setOpen] = useState(false);
+
+  // useState for add modal dialog:
   const [productName, setProductName] = useState('');
   const [productDesc, setProductDesc] = useState('');
   const [supplierName, setSupplierName] = useState('');
   const [unitPrice, setUnitPrice] = useState('');
   const [qtyPerUnit, setQtyPerUnit] = useState('');
   const [maxStock, setMaxStock] = useState('');
+
+  // useState for warnings in case of incorrect/duplicate data:
   const [warning, setWarning] = useState(null);
   const [warningOn, setWarningOn] = useState(false);
 
-  const { table, getData, closeModal, data, openSnackBar } = props;
-  
 
-    // on loading modal/dialog form, we need to load suppliers to add to drop down list
-    useEffect(() => {
-      dispatch(fetchSuppliers());
-    }, []);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const allSupplierNames = useSelector(state => state.catalog.allSuppliers)
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    function handleSubmit(event) {
-      event.preventDefault();
+    //validate information being typed into the form to make sure everything is proper to submit to database
+    const duplicate = [];
+    allProducts.forEach(item => duplicate.push(item.product_name))
 
-      //validate data
-      let duplicate = false;
-      // data.forEach(element => {
-      //   if (element.product_name === productName) return duplicate = true;
-      // })
-      if (duplicate) {
-        setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Product is already in the catalog</Alert>);
-        setWarningOn(true);
-      }
-      if (productName === '') {
-        setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Product name is required</Alert>);
-        setWarningOn(true);
-      }
-      if (supplierName === '') {
-        setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Supplier name is required</Alert>);
-        setWarningOn(true);
-      }
-      if (unitPrice === '') {
-        setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Unit price is required</Alert>);
-        setWarningOn(true);
-      }
-      if (qtyPerUnit === '') {
-        setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Quantity per unit is required</Alert>);
-        setWarningOn(true);
-      }
+    if (duplicate.includes(productName)) {
+      setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Product is already in the catalog</Alert>);
+      setWarningOn(true);
+    }
+    if (productName === '') {
+      setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Product name is required</Alert>);
+      setWarningOn(true);
+    }
+    if (supplierName === '') {
+      setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Supplier name is required</Alert>);
+      setWarningOn(true);
+    }
+    if (unitPrice === '') {
+      setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Unit price is required</Alert>);
+      setWarningOn(true);
+    }
+    if (qtyPerUnit === '') {
+      setWarning(<Alert severity="warning" onClose={() => {setWarningOn(false)}}>Quantity per unit is required</Alert>);
+      setWarningOn(true);
+    } else {
+
+    // create request body to send to database
+    const body = {
+      product_name: productName,
+      product_desc: productDesc, 
+      supplier_name: supplierName,
+      unit_price: unitPrice,
+      qty_per_unit: qtyPerUnit,
+      max_stock: maxStock
+    };
+
+    // dispatch the add post to Catalog action to add item to the catalog store:
+    dispatch(postCatalog(body));
     
-      //create request body
-      else {
-      const body = {
-        product_name: productName,
-        product_desc: productDesc, 
-        supplier_name: supplierName,
-        unit_price: unitPrice,
-        qty_per_unit: qtyPerUnit,
-        max_stock: maxStock
-      };
-
-      // dispatch the add item action to add item to the catalog store:
-      dispatch(postCatalog(body));
-
-      }
+    // close modal on submit button click:
+    setOpen(false);
     }
+  }
 
-    let renderWarning;
-    if (warningOn) {
-      renderWarning = warning;
-    }
-    else renderWarning = null;
+  // warning render if item in form is incorrect:
+  let renderWarning;
+  if (warningOn) {
+    renderWarning = warning;
+  }
+  else renderWarning = null;
 
-      return (
-        <div>
-        <Typography variant="h4">Add Products</Typography>
-        {renderWarning}
-        <form onSubmit={(event) => {handleSubmit(event)}}>
+  return (
+    <div>
+    <Button variant="outlined" onClick={handleClickOpen}>
+      Add Item to Catalog
+    </Button>
+    
+    <Dialog open={open} onClose={handleClose}>
+      {renderWarning}
+      <DialogTitle>New Item to Add</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Enter new product information to add to the catalog.
+        </DialogContentText>
         <TextField
-            label="Product Name"
-            variant="standard"
-            value={productName}
-            onChange={(event) => {
-              setProductName(event.target.value);
-            }} />
+          label="Product Name"
+          variant="standard"
+          value={productName}
+          onChange={(event) => {
+            setProductName(event.target.value);
+          }} />
         <br/>
         <TextField
-            label="Product Description"
-            variant="standard"
-            value={productDesc}
-            onChange={(event) => {
-              setProductDesc(event.target.value);
-            }} />
+          label="Product Description"
+          variant="standard"
+          value={productDesc}
+          onChange={(event) => {
+            setProductDesc(event.target.value);
+          }} />
         <br/>
         <Autocomplete
           options={allSupplierNames}
@@ -110,41 +133,47 @@ function CatalogAddForm (props) {
           }}
           name='supplierName'
           renderInput={params => (
-            <TextField 
-              {...params} 
-              label="Supplier Name" 
-              variant="standard" />
-      )}
-    />
-          <br/>
-          <TextField
-            label="Unit Price"
-            variant="standard"
-            value={unitPrice}
-            onChange={(event) => {
-              setUnitPrice(event.target.value);
-            }} />
-          <br/>
-          <TextField
-            label="Quantity per Unit"
-            variant="standard"
-            value={qtyPerUnit}
-            onChange={(event) => {
-              setQtyPerUnit(event.target.value);
-            }} />
-          <br/>
-          <TextField
-            label="Target Stock Level"
-            variant="standard"
-            value={maxStock}
-            onChange={(event) => {
-              setMaxStock(event.target.value);
-            }}/>
-          <br/>
-          <Button variant="contained" color="primary" type="submit">Submit</Button>
-        </form>
-        </div>
-      );
-}
+          <TextField 
+            {...params} 
+            label="Supplier Name" 
+            variant="standard" />
+          )} />
+        <br/>
+        <TextField
+          label="Unit Price"
+          variant="standard"
+          value={unitPrice}
+          onChange={(event) => {
+            setUnitPrice(event.target.value);
+          }} />
+        <br/>
+        <TextField
+          label="Quantity per Unit"
+          variant="standard"
+          value={qtyPerUnit}
+          onChange={(event) => {
+            setQtyPerUnit(event.target.value);
+          }} />
+        <br/>
+        <TextField
+          label="Target Stock Level"
+          variant="standard"
+          value={maxStock}
+          onChange={(event) => {
+            setMaxStock(event.target.value);
+          }}/>
+        <br/>
+      </DialogContent>
+      
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Submit</Button>
+      </DialogActions>
+    
+    </Dialog>
+  </div>
 
-  export default CatalogAddForm;
+  );
+};
+
+export default CatalogAddForm;
