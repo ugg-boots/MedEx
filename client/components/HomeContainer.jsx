392ec795dfefb50f9a2f3bf5d8d58ec6752ab85b
Alan Ye <alanye@Alans-MBP.fios-router.home>
2021-10-13 21:07:14 -0400

    adding chartjs

--- client/components/HomeContainer.jsx
+++ client/components/HomeContainer.jsx
@@ -3,9 +3,34 @@ import LowStockView from './LowStockView.jsx';
 import ExpirationView from './ExpirationView.jsx';
 import { Link } from 'react-router-dom';
 import LowStockChart from './LowStock/LowStockChart.js';
+import { useState, useEffect } from 'react';
 
 //declared a functional component that will lift the state of ExpirationView
 const HomeContainer = (props)=> {
+
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    
+    setChartData({
+      labels: 'placeholder',
+      datasets: [
+        {
+          label: 'Current Inventory',
+          data: 'placeholder',
+          backgroundColor: '#37b4d4',
+        },
+        {
+          label: 'Target Max Stock',
+          data: 'placeholder',
+          backgroundColor: '#d1349d',
+        },
+
+      ]
+    })
+  }, [])
+
+
   return (
     <div className='homeContainer'>
       <Link to="/">
