13b6f470dff4e05529fcc32387788c6f57d5ad82
Alan Ye <alanye@Alans-MBP.fios-router.home>
2021-10-14 21:21:55 -0400

    working on the chart

--- client/components/HomeContainer.jsx
+++ client/components/HomeContainer.jsx
@@ -9,33 +9,6 @@ import { useSelector } from 'react-redux';
 //declared a functional component that will lift the state of ExpirationView
 const HomeContainer = (props)=> {
 
-  const [chartData, setChartData] = useState({});
-
-  const catalog = useSelector(state => state.catalog.allCatalogItems);
-  const inventory = useSelector(state => state.inventory.displayedInventory)
-  const labels = catalog.map(item => item.product_name);
-  console.log('this is the labels: ', labels)
-
-  useEffect(() => {
-    console.log('this is the labels inside useEffect: ', labels)
-    setChartData({
-      labels: labels,
-      datasets: [
-        {
-          label: 'Current Inventory',
-          data: [1, 5, 2, 4, 2,21,2,2,3,24,6,7],
-          backgroundColor: '#37b4d4',
-        },
-        {
-          label: 'Target Max Stock',
-          data: [5, 20, 4, 2, 1, 4,5,2,2,1,4],
-          backgroundColor: '#d1349d',
-        },
-      ]
-    })
-  }, [])
-
-
   return (
     <div className='homeContainer'>
       <Link to="/">
@@ -44,7 +17,7 @@ const HomeContainer = (props)=> {
       
       <div className='lowStockView'>
         <LowStockView />
-        <LowStockBarChart chartData={chartData} />
+        <LowStockBarChart />
       </div>
     </div>
     )
--- client/components/LowStock/LowStockChart.js
+++ client/components/LowStock/LowStockChart.js
@@ -1,7 +1,76 @@
 import { Bar } from "react-chartjs-2";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
-export const LowStockBarChart = ({ chartData }) => {
+
+export const LowStockBarChart = () => {
+  
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+  const [chartData, setChartData] = useState({});
+
+  const catalog = useSelector(state => state.catalog.allCatalogItems);
+  const inventory = useSelector(state => state.inventory.displayedInventory)
+  const labels = catalog.map(item => item.product_name);
+  console.log('this is the labels outisde useEffect: ', labels)
+
+  const chartConfig = {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Current Inventory',
+          data: [1, 2, 3, 4],
+          backgroundColor: '#37b4d4',
+        },
+        {
+          label: 'Target Max Stock',
+          data: [5, 8, 10, 13],
+          backgroundColor: '#d1349d',
+        },
+      ]
+    },
+
+  }
+
+
+  useEffect(() => {
+    console.log('this is the labels inside useEffect: ', labels)
+    setChartData({
+      labels: labels,
+      datasets: [
+        {
+          label: 'Current Inventory',
+          data: [1, 2, 3, 4],
+          backgroundColor: '#37b4d4',
+        },
+        {
+          label: 'Target Max Stock',
+          data: [5, 8, 10, 13],
+          backgroundColor: '#d1349d',
+        },
+      ],
+      plugins: {
+        title: {
+          display: true,
+          text: 'Target Stock vs. Current Inventory'
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+  }, [])
+
 
   return (
     <div>
